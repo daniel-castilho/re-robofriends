5e1d9951887a5ba17a4ab51dc33902c4a1c1190c
Daniel Castilho <dan.castilho@gmail.com>
2021-03-31 13:49:47 -0400

    Remove console.log()

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const SearchBox = ({ searchField, searchChange }) => {
-    console.log('SearchBox');
     return (
         <div className='pa2'>
             <input
+                aria-label="Search Robots"
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search'
                 placeholder='search robots'
