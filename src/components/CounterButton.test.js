8a3df9afb97c1453cd94a451a154ae636ccb98d1
Daniel Castilho <dan.castilho@gmail.com>
2021-04-01 13:19:54 -0400

    Include one more expect

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -7,11 +7,13 @@ it("renders without crashing", () => {
 });
 
 it("correnctly increments the counter", () => {
-	const wrapper = shallow(<CounterButton />);
+	const mockColor = 'red';
+	const wrapper = shallow(<CounterButton color={ mockColor } />);
 	expect(wrapper).toMatchSnapshot();
 	wrapper.find('[id="counter"]').simulate("click");
 	expect(wrapper.state()).toEqual({ count: 1 });
 	wrapper.find('[id="counter"]').simulate("click");
 	wrapper.find('[id="counter"]').simulate("click");
 	expect(wrapper.state()).toEqual({ count: 3 });
+	expect(wrapper.props().color).toEqual('red');
 });
